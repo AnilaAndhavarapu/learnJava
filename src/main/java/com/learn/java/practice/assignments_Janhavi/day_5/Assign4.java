f1d02e180c60f32ea51d688fb6352b3515e911bd
Sai Kiran <kiran.sai806@gmail.com>
2023-06-26 07:51:51 +0530

    Day 5 assignment updates

---
+++ src/main/java/com/learn/java/practice/assignments_Janhavi/day_5/Assign4.java
@@ -0,0 +1,22 @@
+package com.learn.java.practice.assignments_Janhavi.day_5;
+
+import com.learn.java.practice.utils.Utils;
+
+import java.util.ArrayList;
+
+public class Assign4 {
+
+    public static void main(String[] args) {
+        ArrayList<String> strList = Utils.generateRandomStrings(10);
+        strList.sort(String::compareTo);
+
+        // Write a binary search method to find index of a given word in above sorted String list.
+
+    }
+
+    private static int binarySearchWord(String searchStr){
+
+
+        return 0;
+    }
+}
