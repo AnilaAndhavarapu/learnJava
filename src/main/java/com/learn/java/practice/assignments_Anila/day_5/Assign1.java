85a8ed6889da0c29783d9c612d138a89568f6cff
Sai Kiran <kiran.sai806@gmail.com>
2023-06-28 12:51:11 +0530

    day 4 anila solutions

--- src/main/java/com/learn/java/practice/assignments_Anila/day_5/Assign1.java
+++ src/main/java/com/learn/java/practice/assignments_Anila/day_5/Assign1.java
@@ -1,16 +1,32 @@
 package com.learn.java.practice.assignments_Anila.day_5;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Assign1 {
 
     public static void main(String[] args) {
-        List<String> stringList = Arrays.asList("Pingu", "Tingu", "Jingu", "Tingu", "Mangu");
-
         // create a method which will take a list<String> as input
         // and returns a list of distinct Strings
         // Note: you cannot use list.contains method
         // explore suitable algo / Data Structure for above
+        List<String> stringList = Arrays.asList("Pingu", "Tingu", "Jingu", "Tingu", "Mangu");
+        System.out.println(stringList);
+        System.out.println(distinctStrings(stringList));
+    }
+
+    public static ArrayList<String> distinctStrings(List<String> objList) {
+        ArrayList<String> distinctList = new ArrayList<>();
+        pinguLoop:
+        for (int i = 0; i < objList.size(); i++) {
+            for (int j = 0; j < distinctList.size(); j++) {
+                if (distinctList.get(j).equals(objList.get(i))) {
+                    continue pinguLoop;
+                }
+            }
+            distinctList.add(objList.get(i));
+        }
+        return distinctList;
     }
 }
--- src/main/java/com/learn/java/practice/assignments_Anila/day_5/Assign3.java
+++ src/main/java/com/learn/java/practice/assignments_Anila/day_5/Assign3.java
@@ -3,8 +3,13 @@ package com.learn.java.practice.assignments_Anila.day_5;
 public class Assign3 {
     public static void main(String[] args) {
 
-        String num = "789243";
+        String num = "254";
+        int result = 0;
+
         // convert string num into integer without using parseInt method
-        
+        for (int i = 0; i < num.length(); i++) {
+            result = result * 10 + num.charAt(i) - '0';
+        }
+        System.out.println(result);
     }
 }
