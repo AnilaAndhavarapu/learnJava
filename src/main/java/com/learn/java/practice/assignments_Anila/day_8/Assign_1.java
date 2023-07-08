25fe26afff5a993aa080fb7ccc99ab60182c501f
Mangesh Patil <mangeshpatil11091gmail.com>
2023-07-08 17:07:47 +0530

    Assign Commit
    Signed-off-by: Mangesh Patil <mangeshpatil11091gmail.com>

--- src/main/java/com/learn/java/practice/assignments_Anila/day_8/Assign_1.java
+++ src/main/java/com/learn/java/practice/assignments_Anila/day_8/Assign_1.java
@@ -1,27 +1,20 @@
 package com.learn.java.practice.assignments_Anila.day_8;
 
-public class Assign_1 {
-    public static void main(String[] args) {
-        // Implement doubly linked list
-        CustomDoubleLinkedList list = new CustomDoubleLinkedList();
 
+public class Assign_1 {
+    public static void main(String[] args) throws Exception {
+        // Implement doubly linked list
+        CustomDoubleLinkedList list=new CustomDoubleLinkedList();
         list.add(1);
         print(list);
-
-        list.add(2);
-        print(list);
-
-        list.add(3);
-        print(list);
-
     }
-
-    public static void print(CustomDoubleLinkedList list){
-        for (int i = 0; i < list.getSize(); i++) {
-            System.out.print(list.getAtIndex(i) + ", ");
+    public static void print(CustomDoubleLinkedList list) throws Exception {
+        for(int i=0; i< list.getSize();i++) {
+            System.out.print(list.getAtIndex(i)+" ");
         }
-        System.out.println("Size : " + list.getSize());
+        System.out.print("size"+list.getSize());
     }
+
 }
 
 // Implement
@@ -31,46 +24,85 @@ public class Assign_1 {
 // set at Index methods
 
 class CustomDoubleLinkedList{
-    Node head;
-    Node tail;
-    int count;
+    // Implement
+    // add, addAtIndex
+    // remove element, removeAtIndex
+    // size
+    // set at Index methods
 
-    public void add(int value){
-        Node newNode = new Node(value);
-        if(head == null){
-            head = newNode;
-            tail = head;
-            count++;
-            return;
+    Node head=null;
+    Node tail=null;
+    int count=0;
+
+    public void add(int value)
+    {
+        Node newNode=new Node(value);
+
+        if(head==null)
+        {
+            head=newNode;
+            tail=newNode;
+        }else{
+            tail.setNextNode(newNode);
+            head.setPreviousNode(tail);
+            tail=newNode;
         }
-        Node currNode = tail;
-        tail = newNode;
-        currNode.next = newNode;
-        newNode.prev = currNode;
         count++;
     }
-    public int getAtIndex(int index){
-        Node currNode = head;
-        int counter = 0;
-        while(counter < index){
-           currNode = currNode.next;
-           counter++;
-        }
-        return currNode.value;
-    }
-
-    public int getSize(){
+    public int getSize()
+    {
         return count;
     }
+    public int getAtIndex(int index) throws Exception {
+        if (index > getSize()) {
+            throw new Exception("Index out of Bound  " + index + " for " + getSize());
+        }
+
+        int counter = 0;
+        Node currNode = head;
+        while(counter < index){
+            currNode = currNode.next;
+            counter++;
+        }
+        return currNode.value;
+
+    }
+
+
 }
 
 class Node{
     int value;
     Node next;
-    Node prev;
+    Node previous;
+
+
 
     Node(){}
-    Node(int value){
+
+    public Node(int value){
         this.value = value;
+    }
+
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+    public Node getNextNode(){
+        return next;
+    }
+    public void setNextNode(Node next) {
+        this.next = next;
+    }
+
+    public Node getPreviousNode(){
+        return previous;
+    }
+    public void setPreviousNode(Node previous) {
+        this.previous = previous;
     }
 }
--- src/main/java/com/learn/java/practice/assignments_Janhavi/day_5/Assign2.java
+++ src/main/java/com/learn/java/practice/assignments_Janhavi/day_5/Assign2.java
@@ -1,16 +1,65 @@
 package com.learn.java.practice.assignments_Janhavi.day_5;
 
+import java.lang.String;
+import java.util.Arrays;
+
 public class Assign2 {
 
     public static void main(String[] args) {
         // Anagram --> write a method to check if two strings are anagram of each other or not
         // Example: --> rat and tar are anagrams
+        Assign2 as=new Assign2();
+        as.isAgnagram("rat","tar");
 
+        as.isPalindromes("adda");
         // Panagram --> write a method to check if a given string sentence is panagram or not
         // A panagram is a sentence or string which uses all 26 alphabets.
         // Example : --> “The quick brown fox jumps over the lazy dog”   is a panagram
 
         // Palindrome --> write a method to check if a given string is a palindrome
         // Example : --> abba/ acbca are a palindromes
+    }
+    public void isAgnagram(String s, String s1)
+    {
+
+        char st []= s.toLowerCase().toCharArray();
+
+
+        char st1 []= s1.toLowerCase().toCharArray();
+
+        Arrays.sort(st);
+        Arrays.sort(st1);
+
+
+        if(Arrays.equals(st,st1))
+        {
+            System.out.println("String are anagrams");
+        }
+        else{
+            System.out.println("String are not anagrams");
+        }
+
+    }
+
+    public void isPalindromes(String s)
+    {
+
+        char st []= s.toCharArray();
+
+        char st1[]=new char[st.length];
+
+        for(int i=st1.length-1 ; i>=0;i--)
+        {
+            st1[st.length - 1 - i] = st[i];
+        }
+
+        if(Arrays.equals(st,st1))
+        {
+            System.out.println("String is palindrome");
+        }
+        else{
+            System.out.println("String is not palindrome");
+        }
+
     }
 }
