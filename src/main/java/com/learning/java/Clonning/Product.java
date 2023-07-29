17baca709cbd3872aa140e9e34b47380156577e9
Sai Kiran <kiran.sai806@gmail.com>
2023-07-30 00:09:27 +0530

    Cloning Example

---
+++ src/main/java/com/learning/java/Clonning/Product.java
@@ -0,0 +1,80 @@
+package com.learning.java.Clonning;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+public class Product implements Cloneable{
+
+    private UUID id;
+    private String name;
+    private Double price;
+    private String category;
+    private List<String> productFeatures;
+
+    public Product(UUID id, String name, Double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Product() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setProductFeatures(List<String> productFeatures) {
+        this.productFeatures = productFeatures;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public List<String> getProductFeatures() {
+        return productFeatures;
+    }
+
+    @Override
+    public Product clone(){
+        Product product = new Product();
+        product.name = this.name;
+        product.category = this.category;
+        product.price = this.price;
+        product.id = UUID.randomUUID();
+        if (Objects.nonNull(this.productFeatures)){
+            product.productFeatures = new ArrayList<>(this.productFeatures);
+        }
+        return product;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", category='" + category + '\'' +
+                ", productFeatures=" + productFeatures +
+                '}';
+    }
+}
---
+++ src/main/java/com/learning/java/Clonning/Test.java
@@ -0,0 +1,25 @@
+package com.learning.java.Clonning;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Product product1 = new Product(UUID.randomUUID(), "Bottle", 100.00);
+        List<String> features = new ArrayList<>();
+        features.add("Capacity 1 litre");
+        features.add("Can withstand temp from 0c to 100c");
+        features.add("Easy to clean");
+        product1.setProductFeatures(features);
+        product1.setCategory("Kitchen Utilities");
+
+        System.out.println("Product 1 : " + product1);
+
+        Product product2 = product1.clone();
+        product2.setPrice(200.00);
+        product2.getProductFeatures().set(1, "Can withstand temp from -10c to 150c");
+        System.out.println("Product 1 : " + product2);
+    }
+}
